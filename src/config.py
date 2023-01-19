d41d5bcd4bb8355a140dd9a7de6690bcf5eed35e
Peter Byfield <byfield554@gmail.com>
2023-01-19 16:25:10 +0100

    use default_factory for config values

--- src/config.py
+++ src/config.py
@@ -3,10 +3,18 @@ import os
 import dataclasses
 
 
+def from_env(key: str, f):
+    return dataclasses.field(default_factory=lambda: f(os.environ[key]))
+
+
+def str_from_env(key: str):
+    return from_env(key, lambda x: x)
+
+
 @dataclasses.dataclass(frozen=True)
 class Config:
-    garmin_connect_email: str = os.environ["GARMIN_CONNECT_EMAIL"]
-    garmin_connect_password: str = os.environ["GARMIN_CONNECT_PASSWORD"]
-    strava_email: str = os.environ["STRAVA_EMAIL"]
-    strava_password: str = os.environ["STRAVA_PASSWORD"]
-    n_activities: int = int(os.environ["GARMIN_STRAVA_SYNC_N_ACTIVITIES"])
+    garmin_connect_email: str = str_from_env("GARMIN_CONNECT_EMAIL")
+    garmin_connect_password: str = str_from_env("GARMIN_CONNECT_PASSWORD")
+    strava_email: str = str_from_env("STRAVA_EMAIL")
+    strava_password: str = str_from_env("STRAVA_PASSWORD")
+    n_activities: int = from_env("GARMIN_STRAVA_SYNC_N_ACTIVITIES", int)
