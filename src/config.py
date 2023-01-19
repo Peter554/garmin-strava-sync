d6f62f300b58320a9d99634184117b57da0ce843
Peter Byfield <byfield554@gmail.com>
2023-01-19 16:11:29 +0100

    move config to class

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+
+import dataclasses
+
+
+@dataclasses.dataclass(frozen=True)
+class Config:
+    garmin_connect_email: str = os.environ["GARMIN_CONNECT_EMAIL"]
+    garmin_connect_password: str = os.environ["GARMIN_CONNECT_PASSWORD"]
+    strava_email: str = os.environ["STRAVA_EMAIL"]
+    strava_password: str = os.environ["STRAVA_PASSWORD"]
+    max_activities: int = int(os.environ["GARMIN_STRAVA_SYNC_MAX_ACTIVITIES"])
--- src/sync.py
+++ src/sync.py
@@ -1,4 +1,3 @@
-import os
 import contextlib
 import tempfile
 import logging
@@ -7,29 +6,32 @@ import dotenv
 from playwright.sync_api import sync_playwright
 
 from login import login
+from config import Config
 from download_recent_garmin_runs import download_recent_garmin_runs
 from upload_garmin_runs_to_strava import upload_garmin_runs_to_strava
 
 
 def main():
     dotenv.load_dotenv()  # support .env file for running locally.
+    config = Config()
+
     logging.basicConfig(level=logging.INFO)
 
     with sync_playwright() as p:
         with contextlib.closing(p.chromium.launch(headless=False)) as browser:
             context = login(
                 browser,
-                os.getenv("GARMIN_CONNECT_EMAIL"),
-                os.getenv("GARMIN_CONNECT_PASSWORD"),
-                os.getenv("STRAVA_EMAIL"),
-                os.getenv("STRAVA_PASSWORD"),
+                config.garmin_connect_email,
+                config.garmin_connect_password,
+                config.strava_email,
+                config.strava_password,
             )
             page = context.new_page()
             with tempfile.TemporaryDirectory() as sync_dir:
                 download_recent_garmin_runs(
                     page,
                     sync_dir,
-                    int(os.getenv("GARMIN_STRAVA_SYNC_MAX_ACTIVITIES")),
+                    config.max_activities,
                 )
                 upload_garmin_runs_to_strava(
                     page,
